eb64ac92cfbc7c966d7aaaccaed4dd636921be25
milto <milto@DESKTOP-J104UEV>
2024-06-27 01:07:32 -0300

    Adicionar arquivos de projeto.

---
+++ ZXGames/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ZXGames.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
